12cae973b930b05202e6a7bfd5a42055f1c509e2
Joye <joye.yezhang@gmail.com>
2026-03-30 20:32:06 +0100

    Build profile UI with reusable profile components

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -1,15 +1,130 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
+import Feather from "@expo/vector-icons/Feather";
+import ProfileTab from "@/components/profile/ProfileTab";
+import ProfileHeader from "@/components/profile/ProfileHeader";
 
-const profile = () => {
+const Profile = () => {
+  const [activeTab, setActiveTab] = useState<"posts" | "save">("posts");
+
   return (
-    <ScreenWrapper>
-      <View>
-        <Text>profile</Text>
+    <ScreenWrapper scrollable contentContainerStyle={styles.container}>
+      <ProfileHeader
+        username="Username"
+        bio="BIO"
+        followingCount={0}
+        followersCount={0}
+        onPressEdit={() => {}}
+        onPressSettings={() => {}}
+      />
+
+      <View style={styles.tabRow}>
+        <ProfileTab
+          label="Posts"
+          icon="grid"
+          active={activeTab === "posts"}
+          onPress={() => setActiveTab("posts")}
+        />
+
+        <ProfileTab
+          label="Save"
+          icon="bookmark"
+          active={activeTab === "save"}
+          onPress={() => setActiveTab("save")}
+        />
       </View>
+
+      {activeTab === "posts" ? (
+        <View style={styles.gridPlaceHolder}>
+          <View style={styles.leftColumn}>
+            <View style={styles.largeCard} />
+            <View style={styles.smallCard} />
+            <View style={styles.mediumCard} />
+          </View>
+
+          <View style={styles.rightColumn}>
+            <View style={styles.mediumCard} />
+            <View style={styles.largeCard} />
+            <View style={styles.mediumCard} />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.savedPlaceHolder}>
+          <View style={styles.savedCard} />
+          <View style={styles.savedCard} />
+          <View style={styles.savedCard} />
+          <View style={styles.savedCard} />
+          <View style={styles.savedCard} />
+        </View>
+      )}
     </ScreenWrapper>
   );
 };
 
-export default profile;
+export default Profile;
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    padding: 12,
+  },
+
+  tabRow: {
+    flexDirection: "row",
+    gap: 24,
+    marginBottom: 20,
+  },
+
+  gridPlaceHolder: {
+    // flex: 1,
+    flexDirection: "row",
+    gap: 8,
+  },
+
+  leftColumn: {
+    flex: 1,
+    gap: 8,
+  },
+
+  rightColumn: {
+    flex: 1,
+    gap: 8,
+  },
+
+  largeCard: {
+    height: 240,
+    borderRadius: 12,
+    backgroundColor: "#e7e7e7",
+  },
+
+  mediumCard: {
+    height: 200,
+    borderRadius: 12,
+    backgroundColor: "#e7e7e7",
+  },
+
+  smallCard: {
+    height: 160,
+    borderRadius: 12,
+    backgroundColor: "#e7e7e7",
+  },
+
+  // editButton: {
+  //   width: 24,
+  //   height: 24,
+  //   borderRadius: 12,
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  // },
+
+  savedPlaceHolder: {
+    gap: 8,
+  },
+
+  savedCard: {
+    height: 120,
+    borderRadius: 12,
+    backgroundColor: "#e7e7e7",
+  },
+});
